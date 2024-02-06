21c01f5354a84ba8b25eae5b1ce702d95f37ea3c
Isaiah R <s11670916@students.duvalschools.org>
2024-02-06 14:08:45 -0500

    Update Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int myInt = 0;
+        float myFloat = 3.14F;
+        bool myBool = true
+        string myString = "The FSCJ trip was boring. \n";
+
+        Console.WriteLine("My interger variable is " + myInt);
+        Console.WriteLine("My float variable is " + myFloat);
+        Console.WriteLine("My boolean variable is " + myBool);
+        Console.WriteLine("My string variable is " + myString);
+
+        for (int i = 0; i <= 20; i++)
+        {
+            Console.WriteLine("I ate " + i _ " slices of pizza for lunch.\n");
+        }
+        
     }
 }
